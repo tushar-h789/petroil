7f06e524e43a296aea44844de8e88ed5f19cf7df
tushari789 <95566837+tushari789@users.noreply.github.com>
2022-09-14 23:40:48 +0600

    petroil site is complete

---
+++ JS/talwind.config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        black: "#1F1F1F",
+        lightBlack: "#282828",
+        yellow: "#FFB800",
+        red: "#F40404",
+        alphaOfBlack: "#0009",
+        gray: "#8D8B87",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+      backgroundImage: {
+        banner: "url('images/banner.png')",
+        service_bgOne: "url(images/service1.png)",
+        service_bgTwo: "url(images/service2.png)",
+        service_bgThree: "url(images/service3.png)",
+        about_company: "url('images/about_company.png')",
+        blog_bgOne: "url('images/blog1.png')",
+        blog_bgTwo: "url('images/blog2.png')",
+        blog_bgThree: "url('images/blog3.png')",
+      },
+    },
+  },
+};
